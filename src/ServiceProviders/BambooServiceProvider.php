d028be3ade1edb04409860587b8df0a7f0965a3a
Vahid Kaargar <vahidkaargar@gmail.com>
2023-09-19 14:34:19 +0330

    Change service provider path

--- src/ServiceProviders/BambooServiceProvider.php
+++ src/ServiceProviders/BambooServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace vahidkaargar\BambooCardPortal;
+namespace vahidkaargar\BambooCardPortal\ServiceProviders;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -8,18 +8,18 @@ class BambooServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/bamboo.php', 'bamboo');
+        $this->mergeConfigFrom(__DIR__ . '/../Configs/bamboo.php', 'bamboo');
     }
 
     public function boot()
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__ . '/../config/bamboo.php' => config_path('bamboo.php'),
+                __DIR__ . '/../Configs/bamboo.php' => config_path('bamboo.php'),
             ], 'bamboo-config');
         }
 
-        require_once __DIR__ . '/helpers.php';
+        require_once __DIR__ . '/../Helpers/helpers.php';
     }
 
 }
