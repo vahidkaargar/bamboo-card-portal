0c8a440f0ee2ec9f31845d446e25aaffdbbee40f
Vahid Kaargar <vahidkaargar@gmail.com>
2023-09-20 14:29:04 +0330

    Add PHPDoc

--- tests/unit/BambooTest.php
+++ tests/unit/BambooTest.php
@@ -11,37 +11,58 @@ use vahidkaargar\BambooCardPortal\Tasks\{Account, Catalogs, Exchange, Notificati
  */
 class BambooTest extends TestCase
 {
+    /**
+     * @return void
+     */
     protected function setUp(): void
     {
         parent::setUp();
         $this->bamboo = new Bamboo();
     }
 
+    /**
+     * @return void
+     */
     public function testCatalogsMethodReturnsCatalogInstance()
     {
         $this->assertInstanceOf(Catalogs::class, $this->bamboo->catalogs());
     }
 
+    /**
+     * @return void
+     */
     public function testAccountMethodReturnsAccountInstance()
     {
         $this->assertInstanceOf(Account::class, $this->bamboo->account());
     }
 
+    /**
+     * @return void
+     */
     public function testOrdersMethodReturnsOrderInstance()
     {
         $this->assertInstanceOf(Orders::class, $this->bamboo->orders());
     }
 
+    /**
+     * @return void
+     */
     public function testExchangeMethodReturnsExchangeInstance()
     {
         $this->assertInstanceOf(Exchange::class, $this->bamboo->exchange());
     }
 
+    /**
+     * @return void
+     */
     public function testTransactionsMethodReturnsTransactionsInstance()
     {
         $this->assertInstanceOf(Transactions::class, $this->bamboo->transactions());
     }
 
+    /**
+     * @return void
+     */
     public function testNotificationsMethodReturnsNotificationInstance()
     {
         $this->assertInstanceOf(Notifications::class, $this->bamboo->notifications());
